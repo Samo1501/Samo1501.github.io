0fc12bb8387515a6d6298af7d60d01755032cc3d
Samuel Š <67475168+Samo1501@users.noreply.github.com>
2020-08-21 20:13:32 +0200

    javascripts end of friday

---
+++ Scripting_exercises_4.4.js
@@ -0,0 +1,81 @@
+console.log('Questions:')
+console.log('What will be printed if we run the previous code snippet? Answer: the "else" part of the code, because 7 is more than 5.')
+
+let mynumber = 7; if (mynumber < 5) {console.log(mynumber / 2);} else {console.log(mynumber * 2);}
+
+console.log('What should be changed to get 1 printed to the console? Answer: mynumber2 set from 5 to 2.')
+let mynumber2 = 2; if (mynumber2 < 7) {console.log(mynumber2 / 2);} else {console.log(mynumber2 * 2);}
+
+
+//--------------------------------------------------------------Beginning of exercise 4.4.1 (Loops)--------------------------------------------------------------//
+console.log(' ')
+console.log('Loops exercises')
+
+console.log('0-9:')
+let bookcount0 = 10;
+for (let i = 0; i < bookcount0; i++) {console.log(i)}
+
+console.log(' ')
+
+console.log('1-10:')
+let bookcount1 = 11;
+for (let i = 1; i < bookcount1; i++) {console.log(i)}
+
+console.log(' ')
+
+console.log('0-20 even numbers:')
+let bookcount2 = 20;
+for (let i = 1; i <= bookcount2; i++) {if (i % 2 == 0) {console.log(i);}}
+//--------------------------------------------------------------End of exercise 4.4.1 (Loops)--------------------------------------------------------------//
+
+
+
+
+
+
+//--------------------------------------------------------------Beginning of exercise 4.4.2 (Functions 1)--------------------------------------------------------------//
+console.log('Functions exercise 1a - I have no slightest idea what is the goal here - what are we doing or why')
+// let numtill20 = 20;
+// let printnumbers1 = (numtill20) => {for (let i = 0; i < numtill20; i++) {console.log(i)}};
+// console.log(printnumbers1)
+
+
+console.log('Functions exercise 1b - I have no slightest idea what is the goal here - what are we doing or why')
+// let numtill15 = 15;
+// let printnumbers2 = (numtill15) => {for (let i = 0; i < numtill15; i++) {console.log(i)}};
+// console.log(printnumbers2)
+console.log(' ')
+//-----------------------------------------------------------------End of exercise 4.4.2 (Functions 1)--------------------------------------------------------------//
+
+
+
+
+
+
+//--------------------------------------------------------------Beginning of exercise 4.4.3 (Functions 2)--------------------------------------------------------------//
+console.log('Functions exercise 2 - I understood more or less, but without keeping looking at complete code I would never be able to come to this')
+let person = 'Mark';
+let getgreetingto = (person) => {return "Hello, " + person;}
+console.log(getgreetingto(person),'!')
+//-----------------------------------------------------------------End of exercise 4.4.3 (Functions 2)--------------------------------------------------------------//
+
+
+
+
+
+//--------------------------------------------------------------Beginning of exercise 4.4.4 (For loops and arrays 1)--------------------------------------------------------------//
+
+
+
+//-----------------------------------------------------------------End of exercise 4.4.4 (For loops and arrays 1)--------------------------------------------------------------//
+
+
+
+
+
+
+//--------------------------------------------------------------Beginning of exercise 4.4.5 (For loops and arrays 2)--------------------------------------------------------------//
+
+
+
+//-----------------------------------------------------------------End of exercise 4.4.5 (For loops and arrays 2)--------------------------------------------------------------//
