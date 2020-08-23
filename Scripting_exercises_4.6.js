1c4d4326fe65be78dbf8accfa82a6ab34bde9dc9
Samuel Š <67475168+Samo1501@users.noreply.github.com>
2020-08-23 19:07:26 +0200

    4.6 Exercises given up

---
+++ Scripting_exercises_4.6.js
@@ -0,0 +1,19 @@
+//---------------------------------------------------Beginning of Exercise 1---------------------------------------------------//
+let arraynames = ['John', 'Jimmy', '<strong>Samuel</strong>', 'George', 'Philippe']
+arraynames.forEach(arraynames => {$('.bold').append('<li>', arraynames, '</li>')
+});
+//---------------------------------------------------End of Exercise 1---------------------------------------------------//
+
+//---------------------------------------------------Beginning of Exercise 2---------------------------------------------------//
+console.log(`It is frustrating at every 2nd exercise I have to ask in discord and/or search outside web addresses, just because exercise solutions/documentation links are 
+either broken, outdated or content-useless in relation to exercise tasks. And even if by chance are working, the provided documentation often does not provide ways
+ to solve specific task for an exercise. To be open and honest, javascript part of "My1stCodingCamp" course is not for total beginners at all, at least that's how I see it. 
+ GreenFox might consider to work on that because the effect is needlessly discouraging. If just because of that I have to spend large amounts of time with searches for help 
+ on various 3rd external sites and keep bothering people in discord, what is the point of attending this coding camp course in the first place... I wonder.`)
+
+let additionalBlock = {
+    blocktitle: 'Added with javascript',
+    blocktext: `This block was added using JavaScript's jQuery library. How awesome!`
+};
+
+$('main').html.append(ghjfgjfjfgjhfgj);
