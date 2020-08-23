91fa623b3135e5b45e0e1fba3ad17835e56878fe
Samuel Š <67475168+Samo1501@users.noreply.github.com>
2020-08-23 19:17:50 +0200

    4.6 Given up 2

--- Scripting_exercises_4.6.js
+++ Scripting_exercises_4.6.js
@@ -5,8 +5,8 @@ arraynames.forEach(arraynames => {$('.bold').append('<li>', arraynames, '</li>')
 //---------------------------------------------------End of Exercise 1---------------------------------------------------//
 
 //---------------------------------------------------Beginning of Exercise 2---------------------------------------------------//
-console.log(`It is frustrating at every 2nd exercise I have to ask in discord and/or search outside web addresses, just because exercise solutions/documentation links are 
-either broken, outdated or content-useless in relation to exercise tasks. And even if by chance are working, the provided documentation often does not provide ways
+console.log(`It is frustrating at every 2nd exercise I have to ask in discord and/or search external web sites, just because exercise solutions/documentation links are 
+either broken, outdated or content-useless in relation to given exercise tasks. And even if by chance are working, the provided documentation often does not provide ways
  to solve specific task for an exercise. To be open and honest, javascript part of "My1stCodingCamp" course is not for total beginners at all, at least that's how I see it. 
  GreenFox might consider to work on that because the effect is needlessly discouraging. If just because of that I have to spend large amounts of time with searches for help 
  on various 3rd external sites and keep bothering people in discord, what is the point of attending this coding camp course in the first place... I wonder.`)
@@ -16,4 +16,10 @@ let additionalBlock = {
     blocktext: `This block was added using JavaScript's jQuery library. How awesome!`
 };
 
-$('main').html.append(ghjfgjfjfgjhfgj);
+$('main').html.append();
+
+
+console.log(`I gave up after several hours of trying just to solve these "simple" exercises. I didn't find help fast enough anywhere and I have to complete as much of the rest
+ as I can till 3rd week starts. And because of that I can't afford to wait for an answers in discord as they are far from live... it doesn't work like when I have to wait more 
+ several hours for an answer, as I need to finish it till evening. Javascript part of the My1stCodingCamp should be more detailed inh elp and meant for another week, 
+ definitely not one Friday!!! I have tried play with it even during the weekend and it was just not enough.`)
