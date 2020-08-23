35467e925acb3d7a5e219c91c924c8ed749ac0d9
Samuel Š <67475168+Samo1501@users.noreply.github.com>
2020-08-23 19:18:54 +0200

    Update Scripting_exercises_4.6.js

--- Scripting_exercises_4.6.js
+++ Scripting_exercises_4.6.js
@@ -21,5 +21,5 @@ $('main').html.append();
 
 console.log(`I gave up after several hours of trying just to solve these "simple" exercises. I didn't find help fast enough anywhere and I have to complete as much of the rest
  as I can till 3rd week starts. And because of that I can't afford to wait for an answers in discord as they are far from live... it doesn't work like when I have to wait more 
- several hours for an answer, as I need to finish it till evening. Javascript part of the My1stCodingCamp should be more detailed inh elp and meant for another week, 
- definitely not one Friday!!! I have tried play with it even during the weekend and it was just not enough.`)
+ several hours for an answer, as I need to finish it till evening. Javascript part of the My1stCodingCamp should be more detailed in help and meant for another week, 
+ definitely not for one Friday!!! I have tried play with it even during the weekend and it was just not enough of time.`)
